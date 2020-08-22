9bc9c81ab02271845ade2feef55e597eaedf06d0
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-08-21 21:40:09 -0400

    add routes sauce

---
+++ backend/routes/sauces.js
@@ -0,0 +1,18 @@
+
+const express = require('express');
+const router = express.Router();
+//const saucesControllers = require('../controllers/sauces');
+
+
+/* //router.get('/', saucesControllers.getAllSauces);
+router.get('/:id', saucesControllers.getOneSauce);
+
+// send sauce to DB
+router.post('/', saucesControllers.postOneSauce);
+
+router.put('/:id', saucesControllers.updateOneSauce);
+router.delete('/:id', saucesControllers.getAllSauces); */
+
+
+
+module.exports = router;
