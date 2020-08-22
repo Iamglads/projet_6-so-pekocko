91e49f38d5ed7e7efffded9ecc9919cf7668e086
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-08-21 21:39:46 -0400

    controller sauce

---
+++ backend/controllers/sauces.js
@@ -0,0 +1,22 @@
+const Sauces = require('../models/sauce');
+
+
+exports.getAllSauces = (req, res, next) => {
+
+}
+
+exports.getOneSauce = (req, res, next) => {
+    
+}
+
+exports.postOneSauce = (req, res, next) => {
+    
+}
+
+exports.deleteOneSauce = (req, res, next) => {
+    
+}
+
+exports.updateOneSauce = (req, res, next) => {
+    
+}
