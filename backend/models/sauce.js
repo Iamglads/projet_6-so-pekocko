4180edfb6a9c4d73c4867b94de84e2c44d231a3e
Gladston Aristoverne <gladston.aristoverne.ga@gmail.com>
2020-09-09 15:17:52 -0400

    model sauce ok

--- backend/models/sauce.js
+++ backend/models/sauce.js
@@ -2,16 +2,20 @@ const mongoose = require('mongoose');
 
 const sauceSchema = mongoose.Schema({
 
-    userId : { type: String, require:true, unique:true},
-    name : { type: String, require:true},
-    manufacturer : { type: String, require:true},
-    description : { type: String, require:true, unique:true},
-    mainPepper : { type: String, require:true, unique:true}, // ingrédiant principale 
-    imageUrl : { type: String, require:true},
-    heat : { type: Number, require:true}, // Notre du niveau de piquant de la sauce
-    likes : { type: Number},
-    dislikes : { type: Number},
-    usersLiked : { type: String},
-    usersDilikes : { type: String}
+  
+    name :          { type: String, require: true},
+    manufacturer :  { type: String, require: true},
+    description :   { type: String, require: true},
+    mainPepper :    { type: String, require: true}, // ingrédiant principale 
+    imageUrl :      { type: String, require: true},
+    userId :        { type: String, require: true},
+    heat :          { type: Number, require: true}, // Notre du niveau de piquant de la sauce
+    likes :         { type: Number, default: 0},
+    dislikes :      { type: Number, default: 0},
+    usersLiked :    [{ type: String}],
+    usersDilikes :  [{ type: String}],
 })
+
+
+module.exports = mongoose.model('Sauces', sauceSchema);
 
